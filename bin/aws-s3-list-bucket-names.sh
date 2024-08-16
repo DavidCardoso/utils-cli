b107873c0341321e4cac916ef29c6cffb4f0008e
David Cardoso <david@davidcardoso.me>
2024-08-16 16:52:36 +0200

    fix: Updates aws-s3-list-bucket-names to only output the names (#2)
    - print the list of buckets
    - save the result to a file
    
    closes #1

--- bin/aws-s3-list-bucket-names.sh
+++ bin/aws-s3-list-bucket-names.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-set -e
+# For log purpose when runned remotely (e.g., GitHub raw content URL)
+SERVICE=utils-cli-bin-aws-s3-list-bucket-names
 
-BUCKET_NAMES_FILE="${BUCKET_NAMES_FILE:-buckets.txt}"
+# Args
+BUCKETS_LIST_FILE="${BUCKETS_LIST_FILE:-/tmp/${SERVICE}.log}"
 
-aws s3api list-buckets --query "Buckets[].Name" --output table >$BUCKET_NAMES_FILE
+# Output
+OUTPUT_FILE=${BUCKETS_LIST_FILE}
 
-echo "List of buckets saved to '$BUCKET_NAMES_FILE'"
+# Main command
+response=$(aws s3api list-buckets --query "Buckets[].Name" --output json --no-cli-pager)
+response_code=$?
+
+# Error
+if [[ "${response_code}" != '0' ]]; then
+    echo "${response}" >${OUTPUT_FILE}
+    echo "Service: ${SERVICE}"
+    echo "Error: code=${response_code} ${response}"
+    echo "Logs saved to: '${OUTPUT_FILE}'"
+    exit 1
+fi
+
+# Success
+# gets just the bucket names
+OUTPUT=$(echo "${response}" | jq -r '.[]')
+# print the result and save it in a file
+echo "${OUTPUT}" | tee ${OUTPUT_FILE}
